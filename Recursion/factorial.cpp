64473c63dcaa360651c98ee7ff5bd21ae6084f13
sandeshstha <sandeshshrestha2018@gmail.com>
2019-03-12 14:27:06 +0545

    factorial of a number using recursion

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n)
+{
+	if(n>1)
+	{
+		return n*factorial(n-1);
+	}
+	else
+		return 1;
+}
+int main()
+{
+	int n;
+	cout<<"enter a positive number"<<endl;
+	cin>>n;
+
+	cout<<"the factorial of "<<n<<"is:"<<factorial(n);
+	return 0;
+}
