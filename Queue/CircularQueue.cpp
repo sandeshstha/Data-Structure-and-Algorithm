6d1abd48acfb554261b859208e0ef75c27c6a43f
sandeshstha <sandeshshrestha2018@gmail.com>
2019-03-14 09:52:01 +0545

    circular queue

--- Queue/CircularQueue.cpp
+++ Queue/CircularQueue.cpp
@@ -1,88 +1,111 @@
 #include<iostream>
-#define MAXSIZE 5
+#define MAX 5	
 using namespace std;
 
-int cQueue[MAXSIZE];
-int front=MAXSIZE,rear=MAXSIZE;
-void insert(int data)  //insertion is done from rear
+class CircularQueue
 {
-	if(rear == MAXSIZE-1) //make a place for insertion
+	int items[MAX],front, rear;
+public:
+	CircularQueue()
 	{
-		rear = 0;
+		front = -1;
+		rear = -1;
 	}
-	else
-		rear++; //rear is incremented by 1
-	if(rear == front)
+	bool isFull()
 	{
-		cout<<"cicular Queue is full:"<<endl;
-	}
-	else
-		cQueue[rear] = data;
-}
-void deleteData() //deletion is done from front
-{
-	if(front == MAXSIZE - 1)
-	{
-		front = 0;
-	}
-	else 
-	{
-		front++;
-	}
-	cout<<"element deleted form the cQueue is"<<cQueue[front]<<endl;
-}
-void display()
-{
-	if(front = MAXSIZE -1)
-	{
-		front = 0;
-	}
-	else
-	{
-		front++;
-	}
-	if(front == rear)
-	{
-		cout<<"cicular Queue is empty"<<endl;
-	}
-	else
-	{
-		cout<<"elements of cicular Queue are:"<<endl;
-		for(int i =(front+1);i<=rear;i++)
+		if(front == 0 && rear == MAX-1 || front == rear +1)
 		{
-			cout<<cQueue[i]<<" ";
+			return true;
+		}
+		return false;
+	}
+	bool isEmpty()
+	{
+		if(front == -1)
+		{
+			return true;
+		}
+		else 
+			return false;
+	}
+	void enqueue(int data)
+	{
+		if(isFull())
+		{
+			cout<<"CircularQueue is full"<<endl;
+		}
+		else
+		{
+			if(front == -1)
+			{
+				front = 0; //making a place for first element to insert
+			}
+			rear = (rear+1) % MAX;
+			items[rear] = data;
+			cout<<"inserted data is:"<<data<<endl;
 		}
 	}
-}
+	void dequeue()
+	{
+		int data;
+		if(isEmpty())
+		{
+			cout<<"queue is empty"<<endl;
+		}
+		else
+		{	
+			data = items[front];
+			//if queue have only one element  i.e front == rear
+			if(front == rear)
+			{
+				front = rear = -1; //resetting the queue
+			}
+			else
+			{
+				front = (front + 1)%MAX;
+			}
+			cout<<"deleted item is:"<<data<<endl;
+		}
+	}
+	void display()
+	{
+		int i;
+		if(isEmpty())
+		{
+		 	cout<<"queue is empty:"<<endl;
+		}
+		else
+		{
+			cout<<"the element is queue is:"<<endl;
+			for(i = front;i != rear; i = (i+1)%MAX)
+			{
+				cout<<items[i]<<" ";
+			}
+            cout << items[i]<<endl; //1 item is always left behind 
+
+		}
+	}
+};
+
 int main()
 {
-	int choice,data;
-	cout<<"1)Insert\n";
-    cout<<"2)Delete\n";
-    cout<<"3)Display\n";
-    cout<<"4)Exit\n";
+	CircularQueue q;
+	q.dequeue(); //cannot coz front = -1;
 
-    do{
-    	cout<<"enter your choice:"<<endl;
-    	cin>>choice;
-    	switch(choice)
-    	{
-    		case 1:
-    			cout<<"enter data for insertion:"<<endl;
-    			cin>>data;
-    			insert(data);
-    			break;
-    		case 2:
-    			deleteData();
-    			break;
-    		case 3:
-    			display();
-    			break;
-    		case 4:
-    			exit(1);
-    			break;
-    		default:
-    			cout<<"invalid choice"<<endl;
-    	}
-    }while(choice != 4);
+	q.enqueue(1);
+	q.enqueue(2);
+	q.enqueue(3);
+	q.enqueue(4);
+	q.enqueue(5);
+
+	q.enqueue(6); //fails
+
+	q.display();
+	q.dequeue();
+	q.display();
+	return 0;
+
+
+
 }
+
